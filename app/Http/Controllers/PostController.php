0929304315906cd62523ff2efdc32b5af1ab0ac8
raisrasyiid <raisrasyiid01@gmail.com>
2025-02-24 20:30:33 +0700

    `feat`: add api collection for manipulation data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
 {
     public function index(){
 
         $posts = Post::all();
-        return response()->json($posts);
+        //menggunakan cara biasa 
+        // return response()->json(['data' => $posts]);
+
+        //menggunakan cara API collection
+        return PostResource::collection($posts);
+    }
+
+    public function show($id){
+        $post = Post::findOrFail($id);
+        //menggunakan "new" jika ingin menampilkan data detail (1 data saja)
+        return new PostDetailResource($post);
     }
 }
