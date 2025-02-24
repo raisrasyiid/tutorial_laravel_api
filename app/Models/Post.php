84a20a5df124b88661c0a1641afa9969e688655f
raisrasyiid <raisrasyiid01@gmail.com>
2025-02-24 21:00:37 +0700

    feat: relationship to author table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -12,4 +14,14 @@ class Post extends Model
     protected $fillable = [
         'title', 'news_content', 'author'
     ];
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function writer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author', 'id');
+    }
 }
