8037a2e2d6e746abe723267863a7ff170de20836
raisrasyiid <raisrasyiid01@gmail.com>
2025-02-24 21:00:49 +0700

    `feat`: new routes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/posts', [PostController::class, 'index']); 
+Route::get('/posts/{id}', [PostController::class, 'show']); 
+Route::get('/posts2/{id}', [PostController::class, 'showDetailWithOutWriter']); 
