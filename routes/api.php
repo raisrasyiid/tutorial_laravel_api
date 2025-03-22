b923382fd68f4f4c479feec911a3c12e12fe63bc
raisrasyiid <raisrasyiid01@gmail.com>
2025-03-22 11:50:07 +0700

    `feat`: new API Routes for authentication

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LogController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 
-Route::get('/posts', [PostController::class, 'index']); 
-Route::get('/posts/{id}', [PostController::class, 'show']); 
-Route::get('/posts2/{id}', [PostController::class, 'showDetailWithOutWriter']); 
+#Log-info
+Route::get('/log-api', [LogController::class, 'getData']);
+
+#Authentication
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
+Route::get('/userLogin', [AuthController::class, 'userLogin'])->middleware(['auth:sanctum']);
+
+#Post Features
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']); 
+Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']); 
+
