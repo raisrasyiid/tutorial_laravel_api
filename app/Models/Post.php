6d23c27dd758df7dc8c95594ece11f8602a34d41
raisrasyiid <raisrasyiid01@gmail.com>
2025-03-28 16:28:53 +0700

    `feat`: create softdeletes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title', 'news_content', 'author'
