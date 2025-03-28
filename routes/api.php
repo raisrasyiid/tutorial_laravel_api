ec7918df512f2584603c67855ee5e5ac32db4a97
raisrasyiid <raisrasyiid01@gmail.com>
2025-03-28 16:26:49 +0700

    `feat`: create API middleware and post

--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,20 @@ use App\Http\Controllers\LogController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 
-#Log-info
-Route::get('/log-api', [LogController::class, 'getData']);
-
 #Authentication
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
-Route::get('/userLogin', [AuthController::class, 'userLogin'])->middleware(['auth:sanctum']);
+
+#login
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/userLogin', [AuthController::class, 'userLogin']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware('pemilik-postingan');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->middleware('pemilik-postingan');
+});
 
 #Post Features
-Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']); 
-Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']); 
+Route::get('/posts', [PostController::class, 'index']); 
+Route::get('/posts/{id}', [PostController::class, 'show']); 
+
 
